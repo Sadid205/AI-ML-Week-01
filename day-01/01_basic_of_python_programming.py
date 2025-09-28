7516e9bec6b30c8bd131893d354b027bef2e3840
Md. Abdullah al sadid <abdullahalsadid1914@gmail.com>
2025-09-28 14:49:52 +0600

    Formating number and strings

---
+++ day-01/01_basic_of_python_programming.py
@@ -0,0 +1,67 @@
+# print("Hello")
+# print("World")
+# print("Good Bye")
+# print("<---------------->")
+# print("Hello",end=' ')
+# print("World",end=' ')
+# print("Good Bye",end=' ')
+
+
+# Multiple Assignments
+
+# # Normal Assignment
+# p = 30
+# p = 20
+
+# # Inline Assignment
+# p,q = 20,30
+
+# print(p)
+# print(q)
+
+
+
+# swap number
+
+# p = 20
+# q = 30
+# print(p,q)
+# temp = p
+# p = q 
+# q = temp
+# print(p,q)
+
+
+
+# Using Python Special
+
+# p = 30
+# q = 20
+# p,q = q,p
+# print(p,q)
+
+
+
+# Program 03: Write a simple program to calculate the area of a rectangle in Python
+
+# formula: area = length * breadth
+
+
+
+# length = int(input("Length of rectangle: "))
+# breadth = int(input("Breadth of rectangle: "))
+
+# area = length * breadth
+# print("Area of rectangle is:", area)
+
+
+
+# The eval() Function
+# number = eval(input("Enter a number: "))
+# string = input("Enter a string: ")
+
+# print(type(number))
+# print(type(string))
+
+
+# Formatting number and strings
